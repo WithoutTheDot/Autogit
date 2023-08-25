0a73a431afd1def7bd53d0da3a7ef2cfd1afed43
WithoutTheDot <shieldsjoshua2402@gmail.com>
2023-08-25 13:54:26 +0100

    2 commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ i=1
 f2 = open("script.txt", "r")
 f2 = f2.read().replace("'", "").split(",")
 while True:
-    f = open("main_file.txt", "a")
+    f = open("main_file.txt", "a")      
     word = f2[i]
     print(word)
     f.write(word)
@@ -15,7 +15,7 @@ while True:
     os.system("git add .")
     os.system(f"git commit -m \"{i} commit\"")
     os.system("git push")
-    x=random.randint(2700, 86400)
+    x=random.randint(2.700, 8.6400)
     print(f"Waiting {x/60/60} hours before next commit")
     time.sleep(x) #Between 45mins and 1day
     
